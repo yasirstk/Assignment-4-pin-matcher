4e7ddff2e84430a0df5086a15ec76acf1ba8cf56
yasirstk <yasirstk@users.noreply.github.com>
2020-08-04 03:19:05 +0600

    updated

--- 4th assignment/main.js
+++ 4th assignment/main.js
@@ -1,8 +1,13 @@
 var codeGenerator = document.getElementById("code-generator");
 codeGenerator.addEventListener("click", function(){
     var randNumber = Math.floor(1000 + Math.random()*9000);
-    //console.log(randNumber);
+    
     document.getElementById("generatedCode").value = randNumber;
-    
-    
 })
+
+var submittedPin = document.getElementById("screen").value;
+document.getElementById("pin-submit").addEventListener("click",function(){
+    
+
+})
+       
